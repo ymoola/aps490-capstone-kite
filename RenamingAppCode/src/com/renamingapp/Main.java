c57207bb9d13ebdac9b6202e2844b9765d47c6ba
archana0127 <a.rajendran@mail.utoronto.ca>
2025-11-12 22:01:27 -0500

    Made starter files

---
+++ RenamingAppCode/src/com/renamingapp/Main.java
@@ -0,0 +1,36 @@
+package com.renamingapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+public class Main extends Application {
+
+    @Override
+    public void start (Stage primaryStage){
+        try {
+             // Load your first FXML file (the welcome page)
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/renamingapp/fxml/welcome.fxml"));
+            Parent root = (Parent)fxmlLoader.load();
+            Scene scene = new Scene(root);
+
+             // Set window title
+            primaryStage.setTitle("File Renaming App");
+            // Set and show the scene
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+
+
+    public static void main(String[] args) {
+        launch(args); // Launches JavaFX application
+    }
+    
+}
+
+
